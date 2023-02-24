47753ecef5fb44ff43fddfd0cb2f61c0272a803e
Ruslan Himadieiev <gimadejev86@gmail.com>
2023-02-24 15:54:50 +0200

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function getCurrentTime({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(currentTime);
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
 
