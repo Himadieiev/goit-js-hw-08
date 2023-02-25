4db7b0265d4ecfe45b5aa7079c38a83eb7b39db1
Ruslan Himadieiev <gimadejev86@gmail.com>
2023-02-25 13:51:18 +0200

    add

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const formData = {};
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+populateForm();
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function populateForm() {
+  const savedInputForm = JSON.parse(localStorage.getItem('feedback-form-state'));
+  
+  if (savedInputForm !== {}) {
+    input.value = savedInputForm.email;
+    textarea.value = savedInputForm.message;
+  }
+}
